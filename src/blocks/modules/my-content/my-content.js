9fe071f13c756b8cd2b19f4ffbd8b0336a7f2c39
Nickolas <nikolyae2019@gmail.com>
2019-07-24 22:51:19 +0300

    commit

--- src/blocks/modules/my-content/my-content.js
+++ src/blocks/modules/my-content/my-content.js
@@ -4,6 +4,8 @@ $(function(){
     $('.owl-carousel').owlCarousel({
         nav: true,
         loop:true,
-        dots: true
+        dots: true,
+        smartSpeed: 700,
+        navText: ['<i class="icon icofont-double-left"</i>','<i class="icon icofont-double-right"</i>']
     });
 });
