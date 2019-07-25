abe0e8f4a3ea8dceb9f1121a6918f7a6d654207a
Nickolas <nikolyae2019@gmail.com>
2019-07-25 22:55:15 +0300

    commit

--- src/blocks/modules/my-content/my-content.js
+++ src/blocks/modules/my-content/my-content.js
@@ -6,6 +6,31 @@ $(function(){
         loop:true,
         dots: true,
         smartSpeed: 700,
-        navText: ['<i class="icon icofont-double-left"</i>','<i class="icon icofont-double-right"</i>']
+        navText: ['<i class="icon icofont-double-left"</i>','<i class="icon icofont-double-right"</i>'],
+        responsiveClass: true,
+        responsive:{ // @media
+            0:{
+                items:1, // one items on this size
+                nav:true
+            },
+            800:{ // size
+                items:2,
+                nav:false
+            },
+            1100:{ // size
+                items:3,
+                nav:true,
+            }
+        }
     });
+
+    function carouselService() {
+        $('.carousel-services-item').each(function() {
+            var ths = $(this),
+            thsh = ths.find('.carousel-services-content').outerHeight();
+            ths.find('.carousel-services-image').css('min-height', thsh);
+        });
+    } carouselService()
+
 });
+
