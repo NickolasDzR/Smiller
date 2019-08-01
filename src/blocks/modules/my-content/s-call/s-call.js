46896a6cb5920761e440bb71ba0d073c22a8627d
Nickolas <nikolyae2019@gmail.com>
2019-08-01 18:58:01 +0300

    commit

--- src/blocks/modules/my-content/s-call/s-call.js
+++ src/blocks/modules/my-content/s-call/s-call.js
@@ -1,7 +1,24 @@
 import $ from 'jquery';
 
-$(function(){
+$(function () {
     $('select').selectize({
-    create: true,
+        create: true,
     })
-;})
+
+    $("form.call-back").submit(function () { //Change
+        var th = $(this);
+        $.ajax({
+            type: "POST",
+            url: "/dist/mail.php", //Change
+            data: th.serialize()
+        }).done(function () {
+            $(th).find('.success').addClass('active').css('display', 'flex').hide().fadeIn();
+            setTimeout(function () {
+                $(th).find(".success").removeClass('active').fadeOut();
+                // Done Functions
+                th.trigger("reset");
+            }, 3000);
+        });
+        return false;
+    });;
+})
