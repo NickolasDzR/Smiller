727209c08167786892e5c11182b48f95b8787372
Nickolas <nikolyae2019@gmail.com>
2019-08-02 11:35:05 +0300

    Update s-review.js

--- src/blocks/modules/my-content/s-review/s-review.js
+++ src/blocks/modules/my-content/s-review/s-review.js
@@ -1,13 +1,21 @@
 import $ from 'jquery';
 
-
+var owl = $('.s-reviews .owl-carousel');
 $(function () {
     $('.reviews').owlCarousel({
         loop: true,
         items: 1,
-        smartSpeed: 300,
+        smartSpeed: 1500,
         nav: false,
         dots: true,
         autoHeight: true
+    });
+    owl.on('mousewheel', '.owl-stage', function (e) {
+        if (e.deltaY>0) {
+            owl.trigger('next.owl');
+        } else {
+            owl.trigger('prev.owl');
+        }
+        e.preventDefault();
     });
 });
