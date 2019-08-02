8efcf85eb23df7b451c1c373629b32581b418c55
Nickolas <nikolyae2019@gmail.com>
2019-08-02 14:00:26 +0300

    Partners section

---
+++ src/blocks/modules/my-content/s-partners/s-partners.js
@@ -0,0 +1,37 @@
+import $ from 'jquery';
+
+var owl = $('.s-partners .owl-carousel');
+$(function () {
+    $('.partners').owlCarousel({
+        loop: true,
+        items: 4,
+        smartSpeed: 1500,
+        nav: true,
+        navText: ['<i class="icon icofont-rounded-left"</i>','<i class="icon icofont-rounded-right"</i>'],
+        dots: false,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            768: {
+                items: 2
+            },
+            992: {
+                items: 3
+            },
+            1200: {
+                items: 4
+            },
+        }
+    });
+    owl.on('mousewheel', '.owl-stage', function (e) {
+        if (e.deltaY>0) {
+            owl.trigger('next.owl');
+        } else {
+            owl.trigger('prev.owl');
+        }
+        e.preventDefault();
+    });
+});
+
--- src/js/import/modules.js
+++ src/js/import/modules.js
@@ -3,4 +3,5 @@ import "%modules%/my-content/my-content.js";
 import "%modules%/my-content/s-king/s-king.js";
 import "%modules%/my-content/s-call/s-call.js";
 import "%modules%/my-content/s-review/s-review.js";
+import "%modules%/my-content/s-partners/s-partners.js";
 import "%modules%/my-footer/my-footer.js"
