2708b893467e969c34c3411149460f1dc2af2b17
Nickolas <nikolyae2019@gmail.com>
2019-08-02 11:29:11 +0300

    quotes section

--- src/blocks/modules/my-content/s-review/s-review.js
+++ src/blocks/modules/my-content/s-review/s-review.js
@@ -2,6 +2,12 @@ import $ from 'jquery';
 
 
 $(function () {
-    $('.review').owlCarousel({
+    $('.reviews').owlCarousel({
+        loop: true,
+        items: 1,
+        smartSpeed: 300,
+        nav: false,
+        dots: true,
+        autoHeight: true
     });
 });
